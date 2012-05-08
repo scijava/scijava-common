0ab66abc72134653dea431245f465ab99fc69336
Curtis Rueden <ctrueden@wisc.edu>
2012-05-08 14:55:23 -0500

    Use a default remote when none is given

--- bin/remote-branch-info.sh
+++ bin/remote-branch-info.sh
@@ -5,16 +5,26 @@
 #
 # This is useful for analyzing which branches are obsolete and/or moldy.
 
-remote="$@"
+remote="$*"
 if [ "$remote" == "" ];
 then
+  remote=$(git rev-parse --symbolic-full-name HEAD@{u})
+  remote=${remote%/*}
+  remote=${remote#refs/remotes/}
+  echo Using remote $remote
+fi
+
+case "$remote" in
+--help|-h)
   echo Please specify one of the following remotes:
   git remote
   exit 1
-fi
+  ;;
+esac
 
 for ref in $(git for-each-ref refs/remotes/$remote --format='%(refname)')
 do
+
   refname=${ref#refs/remotes/$remote/}
   unmerged_count=$(git cherry master $ref | grep '^+' | wc -l)
   info=$(git log -n 1 --format='%an - %ar' $ref)
