20e9e569366e03732c8606b9480241fc99a00290
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-04-07 19:24:53 -0500

    Verify that ProcessUtils#exec can be interrupted
    This test requires the 'sleep' program, therefore we only test it on
    Linux and MacOSX, but not on Windows.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ src/test/java/org/scijava/util/ProcessUtilsTest.java
@@ -0,0 +1,96 @@
+/*
+ * #%L
+ * SciJava Common shared library for SciJava software.
+ * %%
+ * Copyright (C) 2009 - 2013 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package org.scijava.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assume.assumeTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link ProcessUtils}.
+ * 
+ * @author Johannes Schindelin
+ */
+public class ProcessUtilsTest {
+
+	@Test
+	public void testInterruptible() throws InterruptedException {
+		assumePOSIX();
+		final SleepThread thread = new SleepThread(5000);
+		thread.start();
+		Thread.sleep(100);
+		thread.interrupt();
+		thread.join();
+		assertNotNull(thread.getResult());
+	}
+
+	private void assumePOSIX() {
+		final String osName = System.getProperty("os.name");
+		assumeTrue(osName.equals("Linux") || osName.equals("Mac OS X"));
+	}
+
+	/**
+	 * A class executing a 'sleep' call, to be interrupted.
+	 * 
+	 * @author Johannes Schindelin
+	 */
+	private static class SleepThread extends Thread {
+		private int seconds;
+		private Throwable result;
+
+		public SleepThread(int seconds) {
+			this.seconds = seconds;
+		}
+
+		@Override
+		public void run() {
+			try {
+				ProcessUtils.exec(null, null, null, "sleep", "" + seconds);
+			} catch (Throwable t) {
+				result = t;
+			}
+		}
+
+		public Throwable getResult() {
+			return result;
+		}
+	}
+}
