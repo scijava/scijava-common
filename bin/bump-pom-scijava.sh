d2ddec1edeca0d70647792bf6f7a2a797e213e3f
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-04-11 16:44:47 -0500

    bump-pom-scijava: fix inverted logic
    We *want* pom.xml to differ from pom.xml.new after increasing the
    version number...
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/bump-pom-scijava.sh
+++ bin/bump-pom-scijava.sh
@@ -182,7 +182,7 @@ mv $pom $pom.new &&
 sed \
   -e "s/^\(\\t<version>\)$old_version\(<\/version>\)/\1$new_version\2/" \
   $pom.new > $pom &&
-! git diff --quiet --no-index $pom $pom.new ||
+git diff --quiet --no-index $pom $pom.new ||
 die "Failed to increase version of $pom"
 
 rm $pom.new ||
