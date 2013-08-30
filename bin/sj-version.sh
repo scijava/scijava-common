2da596cb61b203606acde276f183a8be7719c057
Curtis Rueden <ctrueden@wisc.edu>
2013-08-30 10:55:48 -0500

    sj-version.sh: add ability to compare two versions

--- bin/sj-version.sh
+++ bin/sj-version.sh
@@ -2,19 +2,37 @@
 
 # Script to print version properties for a given pom-scijava release.
 
-# Example: sj-version.sh 1.70
+# Examples:
+# sj-version.sh 1.70
+# sj-version.sh 1.70 1.74
 
 version="$1"
+diff="$2"
+
+repo="http://maven.imagej.net/content/repositories/releases"
+
+props() {
+	url="$repo/org/scijava/pom-scijava/$1/pom-scijava-$1.pom"
+	curl -s $url | \
+		grep '\.version' | \
+		sed 's/<\/.*//' | \
+		sed 's/^	*<\(.*\)>/\1: /' | \
+		sort
+}
 
 if [ -z "$version" ]
 then
-  echo "Usage: sj-version.sh version"
+  echo "Usage: sj-version.sh version [version-to-diff]"
 fi
 
-repo="http://maven.imagej.net/content/repositories/releases"
-url="$repo/org/scijava/pom-scijava/$version/pom-scijava-$version.pom"
-curl -s $url | \
-	grep '\.version' | \
-	sed 's/<\/.*//' | \
-	sed 's/^	*<\(.*\)>/\1: /' | \
-	sort
+if [ -n "$diff" ]
+then
+	# compare two versions
+	props $version > $version.tmp
+	props $diff > $diff.tmp
+	diff -y $version.tmp $diff.tmp
+	rm $version.tmp $diff.tmp
+else
+	# dump props for one version
+	props $version
+fi
