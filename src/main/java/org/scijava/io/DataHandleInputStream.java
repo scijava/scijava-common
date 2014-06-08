f102265452c256ddb9e61622c4f5b7d20662d1f9
Curtis Rueden <ctrueden@wisc.edu>
2014-06-07 21:54:27 -0500

    Migrate SCIFIO I/O framework to SciJava Common
    This implementation is a restructuring of SCIFIO's io.scif.io package,
    to fit within the SciJava plugin framework.
    
    This commit introduces a new plugin type, DataHandle, that defines all
    the methods for random access reading and writing. The DataHandle
    interface is a rich SciJava plugin type that extends the
    java.io.DataInput, java.io.DataOutput and java.io.Closeable interfaces.
    
    Furthermore, the DataHandle interface also defines additional API
    methods that make it very straightforward to adapt DataHandle objects
    into InputStreams and OutputStreams (it cannot extend InputStream and
    OutputStream, because they are abstract classes, not interfaces).
    
    The actual adaptation to streams is done using the DataHandleInputStream
    and DataHandleOutputStream wrapper objects, simply by delegating to the
    various DataHandle methods of the same name.
    
    Overall, the mapping from the old io.scif.io API is as follows:
    
    * IRandomAccess + IStreamAccess -> org.scijava.io.DataHandle
    * RandomAccessInputStream -> org.scijava.io.DataHandleInputStream
    * RandomAccessOutputStream -> org.scijava.io.DataHandleOutputStream
    * Location -> subsumed into the org.scijava.io.Location type hierarchy
    * HandleException -> unnecessary; IOException et. al are good enough
    * FileHandle, URLHandle, etc. -> same names, implementing DataHandle
    
    Note that the mapping is rather loose, with several layers consolidated
    or eliminated. Additional commits will readd features, such as specific
    DataHandle plugins, as appropriate to achieve needed functionality.

---
+++ src/main/java/org/scijava/io/DataHandleInputStream.java
@@ -0,0 +1,119 @@
+/*
+ * #%L
+ * SciJava Common shared library for SciJava software.
+ * %%
+ * Copyright (C) 2009 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * {@link InputStream} backed by a {@link DataHandle}.
+ * 
+ * @author Curtis Rueden
+ * @author Melissa Linkert
+ */
+public class DataHandleInputStream<L extends Location> extends InputStream {
+
+	// -- Fields --
+
+	private final DataHandle<L> handle;
+
+	private long mark = -1;
+
+	// -- Constructors --
+
+	/** Creates an input stream around the given {@link DataHandle}. */
+	public DataHandleInputStream(final DataHandle<L> handle) {
+		this.handle = handle;
+	}
+
+	// -- DataHandleInputStream methods --
+
+	public DataHandle<L> getDataHandle() {
+		return handle;
+	}
+
+	// -- InputStream methods --
+
+	@Override
+	public int read() throws IOException {
+		return handle.read();
+	}
+
+	@Override
+	public int read(final byte[] array, final int offset, final int n)
+		throws IOException
+	{
+		return handle.read(array, offset, n);
+	}
+
+	@Override
+	public long skip(final long n) throws IOException {
+		return handle.skip(n);
+	}
+
+	@Override
+	public int available() throws IOException {
+		long remain = handle.length() - handle.offset();
+		if (remain > Integer.MAX_VALUE) remain = Integer.MAX_VALUE;
+		return (int) remain;
+	}
+
+	@Override
+	public synchronized void mark(final int readLimit) {
+		try {
+			mark = handle.offset();
+		}
+		catch (final IOException exc) {
+			throw new IllegalStateException(exc);
+		}
+	}
+
+	@Override
+	public synchronized void reset() throws IOException {
+		if (mark < 0) throw new IOException("No mark set");
+		handle.seek(mark);
+	}
+
+	@Override
+	public boolean markSupported() {
+		return true;
+	}
+
+	// -- Closeable methods --
+
+	@Override
+	public void close() throws IOException {
+		handle.close();
+		mark = -1;
+	}
+
+}
