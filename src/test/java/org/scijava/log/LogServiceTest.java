e82c70493ad686cd32a66129cfeb30980bac9a63
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-06-10 10:45:37 -0500

    Fix test: we now expect the default log level to be WARN instead...
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src/test/java/org/scijava/log/LogServiceTest.java
+++ src/test/java/org/scijava/log/LogServiceTest.java
@@ -32,7 +32,7 @@
 package org.scijava.log;
 
 import static org.junit.Assert.assertTrue;
-import static org.scijava.log.LogService.INFO;
+import static org.scijava.log.LogService.WARN;
 
 import org.junit.Test;
 
@@ -46,6 +46,6 @@ public class LogServiceTest {
 	public void testDefaultLevel() {
 		final LogService log = new StderrLogService();
 		int level = log.getLevel();
-		assertTrue("default level (" + level + ") is at least INFO(" + INFO + ")", level >= INFO);
+		assertTrue("default level (" + level + ") is at least INFO(" + WARN + ")", level >= WARN);
 	}
 }
