4e1343689567e31a5171a1f008f07b7e97d79387
Mark Hiner <hinerm@gmail.com>
2014-07-25 07:48:07 -0500

    Create ConversionService
    Adds a new HandlerService: ConversionService, operating on
    ConversionHandler plugins.
    
    The purpose of these handlers is to process a new class,
    "ConversionRequest", which signifies the desire to cast or convert an
    object of one type to another.
    
    This will take the place of ConversionUtils, as it is an extensible
    framework. ConversionHandler plugins can decide what types of conversion
    they support and thus override the default behavior (the default plugin
    supports primitive : object conversions and cases where the destination
    object has a constructor that will accept the source, for example).
    
    Closes https://github.com/scijava/scijava-common/issues/95

---
+++ src/main/java/org/scijava/util/conversion/ConversionHandler.java
@@ -0,0 +1,158 @@
+/*
+ * #%L
+ * SciJava Common shared library for SciJava software.
+ * %%
+ * Copyright (C) 2009 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.util.conversion;
+
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import org.scijava.plugin.HandlerPlugin;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Extensible conversion {@link Plugin} for converting between classes and
+ * types.
+ *
+ * @see ConversionRequest
+ * @author Mark Hiner
+ */
+public interface ConversionHandler extends HandlerPlugin<ConversionRequest> {
+
+	/**
+	 * Checks whether a given {@ConversionRequest} can be
+	 * processed, by converting the desired
+	 * {@link ConversionRequest#sourceClass()} to its
+	 * {@link ConversionRequest#destClass()} or
+	 * {@link ConversionRequest#destType()}.
+	 *
+	 * @see #convert(ConversionRequest)
+	 */
+	boolean canConvert(ConversionRequest request);
+
+	/**
+	 * Checks whether objects of the given class can be converted to the specified
+	 * type.
+	 *
+	 * @see #convert(Object, Type)
+	 */
+	boolean canConvert(Class<?> src, Type dest);
+
+	/**
+	 * Checks whether objects of the given class can be converted to the specified
+	 * type.
+	 *
+	 * @see #convert(Object, Class)
+	 */
+	boolean canConvert(Class<?> src, Class<?> dest);
+
+	/**
+	 * Checks whether the given object's type can be converted to the specified
+	 * type.
+	 * <p>
+	 * Note that this does <em>not</em> necessarily entail that
+	 * {@link #convert(Object, Type)} on that specific object will succeed. For
+	 * example: {@code canConvert("5.1", int.class)} will return {@code true}
+	 * because a {@link String} can in general be converted to an {@code int}, but
+	 * calling {@code convert("5.1", int.class)} will throw a
+	 * {@link NumberFormatException} when the conversion is actually attempted via
+	 * the {@link Integer#Integer(String)} constructor.
+	 * </p>
+	 *
+	 * @see #convert(Object, Type)
+	 */
+	boolean canConvert(Object src, Type dest);
+
+	/**
+	 * Checks whether the given object's type can be converted to the specified
+	 * type.
+	 * <p>
+	 * Note that this does <em>not</em> necessarily entail that
+	 * {@link #convert(Object, Class)} on that specific object will succeed. For
+	 * example: {@code canConvert("5.1", int.class)} will return {@code true}
+	 * because a {@link String} can in general be converted to an {@code int}, but
+	 * calling {@code convert("5.1", int.class)} will throw a
+	 * {@link NumberFormatException} when the conversion is actually attempted via
+	 * the {@link Integer#Integer(String)} constructor.
+	 * </p>
+	 *
+	 * @see #convert(Object, Class)
+	 */
+	boolean canConvert(Object src, Class<?> dest);
+
+	/**
+	 * As {@link #convert(Object, Class)} but capable of creating and populating
+	 * multi-element objects ({@link Collection}s and array types). If a single
+	 * element type is provided, it will be converted the same as
+	 * {@link #convert(Object, Class)}. If a multi-element type is detected, then
+	 * the value parameter will be interpreted as potential collection of values.
+	 * An appropriate container will be created, and the full set of values will
+	 * be type converted and added.
+	 * <p>
+	 * NB: This method should be capable of creating any array type, but if a
+	 * {@link Collection} interface or abstract class is provided we can only make
+	 * a best guess as to what container type to instantiate. Defaults are
+	 * provided for {@link Set} and {@link List} subclasses.
+	 * </p>
+	 *
+	 * @param src The object to convert.
+	 * @param dest Type to which the object should be converted.
+	 */
+	Object convert(Object src, Type dest);
+
+	/**
+	 * Converts the given object to an object of the specified type. The object is
+	 * casted directly if possible, or else a new object is created using the
+	 * destination type's public constructor that takes the original object as
+	 * input (except when converting to {@link String}, which uses the
+	 * {@link Object#toString()} method instead). In the case of primitive types,
+	 * returns an object of the corresponding wrapped type. If the destination
+	 * type does not have an appropriate constructor, returns null.
+	 *
+	 * @param <T> Type to which the object should be converted.
+	 * @param src The object to convert.
+	 * @param dest Type to which the object should be converted.
+	 */
+	<T> T convert(Object src, Class<T> dest);
+
+	/**
+	 * Converts the given {@link ConversionRequest#sourceObject()} to the
+	 * specified {@link ConversionRequest#destClass()} or
+	 * {@link ConversionRequest#destType()}.
+	 *
+	 * @see #convert(Object, Class)
+	 * @see #convert(Object, Type)
+	 * @param request {@link ConversionRequest} to process.
+	 * @return The conversion output
+	 */
+	Object convert(ConversionRequest request);
+}
---
+++ src/main/java/org/scijava/util/conversion/DefaultConversionHandler.java
@@ -0,0 +1,360 @@
+/*
+ * #%L
+ * SciJava Common shared library for SciJava software.
+ * %%
+ * Copyright (C) 2009 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.util.conversion;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.scijava.plugin.Plugin;
+import org.scijava.util.ArrayUtils;
+import org.scijava.util.ClassUtils;
+import org.scijava.util.ConversionUtils;
+import org.scijava.util.GenericUtils;
+
+/**
+ * Default {@link ConversionHandler} implementation. Provides useful conversion
+ * functionality for many common conversion cases.
+ *
+ * @author Mark Hiner
+ */
+@Plugin(type = ConversionHandler.class)
+public class DefaultConversionHandler extends AbstractConversionHandler {
+
+	// -- ConversionHandler methods --
+
+	@Override
+	public boolean canConvert(final Class<?> src, final Type dest) {
+		// NB: Regardless of whether the destination type is an array or collection,
+		// we still want to cast directly if doing so is possible. But note that in
+		// general, this check does not detect cases of incompatible generic
+		// parameter types. If this limitation becomes a problem in the future we
+		// can extend the logic here to provide additional signatures of canCast
+		// which operate on Types in general rather than only Classes. However, the
+		// logic could become complex very quickly in various subclassing cases,
+		// generic parameters resolved vs. propagated, etc.
+		final Class<?> c = GenericUtils.getClass(dest);
+		if (c != null && ConversionUtils.canCast(src, c)) return true;
+
+		// Handle array types, including generic array types.
+		if (isArray(dest)) return true;
+
+		// Handle parameterized collection types.
+		if (dest instanceof ParameterizedType && isCollection(dest)) {
+			return createCollection(GenericUtils.getClass(dest)) != null;
+		}
+
+		// This wasn't a collection or array, so convert it as a single element.
+		return canConvert(src, GenericUtils.getClass(dest));
+	}
+
+	@Override
+	public boolean canConvert(final Class<?> src, final Class<?> dest) {
+		// ensure type is well-behaved, rather than a primitive type
+		final Class<?> saneDest = ConversionUtils.getNonprimitiveType(dest);
+
+		// OK if the existing object can be casted
+		if (ConversionUtils.canCast(src, saneDest)) return true;
+
+		// OK for numerical conversions
+		if (ConversionUtils.canCast(ConversionUtils.getNonprimitiveType(src),
+			Number.class) &&
+			(ClassUtils.isByte(dest) || ClassUtils.isDouble(dest) ||
+				ClassUtils.isFloat(dest) || ClassUtils.isInteger(dest) ||
+				ClassUtils.isLong(dest) || ClassUtils.isShort(dest)))
+		{
+			return true;
+		}
+
+		// OK if string
+		if (saneDest == String.class) return true;
+
+		if (ConversionUtils.canCast(src, String.class)) {
+			// OK if source type is string and destination type is character
+			// (in this case, the first character of the string would be used)
+			if (saneDest == Character.class) return true;
+
+			// OK if source type is string and destination type is an enum
+			if (dest.isEnum()) return true;
+		}
+
+		// OK if appropriate wrapper constructor exists
+		try {
+			return getConstructor(saneDest, src) != null;
+		}
+		catch (final Exception exc) {
+			// TODO: Best not to catch blanket Exceptions here.
+			// no known way to convert
+			return false;
+		}
+	}
+
+	@Override
+	public boolean canConvert(final Object src, final Type dest) {
+		if (src == null) return true;
+		return canConvert(src.getClass(), dest);
+	}
+
+	@Override
+	public boolean canConvert(final Object src, final Class<?> dest) {
+		if (src == null) return true;
+		return canConvert(src.getClass(), dest);
+	}
+
+	@Override
+	public Object convert(final Object src, final Type dest) {
+		// NB: Regardless of whether the destination type is an array or collection,
+		// we still want to cast directly if doing so is possible. But note that in
+		// general, this check does not detect cases of incompatible generic
+		// parameter types. If this limitation becomes a problem in the future we
+		// can extend the logic here to provide additional signatures of canCast
+		// which operate on Types in general rather than only Classes. However, the
+		// logic could become complex very quickly in various subclassing cases,
+		// generic parameters resolved vs. propagated, etc.
+		final Class<?> c = GenericUtils.getClass(dest);
+		if (c != null && ConversionUtils.canCast(src, c)) return ConversionUtils
+			.cast(src, c);
+
+		// Handle array types, including generic array types.
+		if (isArray(dest)) {
+			return convertToArray(src, GenericUtils.getComponentClass(dest));
+		}
+
+		// Handle parameterized collection types.
+		if (dest instanceof ParameterizedType && isCollection(dest)) {
+			return convertToCollection(src, (ParameterizedType) dest);
+		}
+
+		// This wasn't a collection or array, so convert it as a single element.
+		return convert(src, GenericUtils.getClass(dest));
+	}
+
+	@Override
+	public <T> T convert(final Object src, final Class<T> dest) {
+		// TODO: Would be better to split up this method into some helpers.
+		if (dest == null) return null;
+		if (src == null) return ConversionUtils.getNullValue(dest);
+
+		// ensure type is well-behaved, rather than a primitive type
+		final Class<T> saneDest = ConversionUtils.getNonprimitiveType(dest);
+
+		// cast the existing object, if possible
+		if (ConversionUtils.canCast(src, saneDest)) return ConversionUtils.cast(
+			src, saneDest);
+
+		// special case for conversion from number to number
+		if (src instanceof Number) {
+			final Number number = (Number) src;
+			if (saneDest == Byte.class) {
+				final Byte result = number.byteValue();
+				@SuppressWarnings("unchecked")
+				final T typedResult = (T) result;
+				return typedResult;
+			}
+			if (saneDest == Double.class) {
+				final Double result = number.doubleValue();
+				@SuppressWarnings("unchecked")
+				final T typedResult = (T) result;
+				return typedResult;
+			}
+			if (saneDest == Float.class) {
+				final Float result = number.floatValue();
+				@SuppressWarnings("unchecked")
+				final T typedResult = (T) result;
+				return typedResult;
+			}
+			if (saneDest == Integer.class) {
+				final Integer result = number.intValue();
+				@SuppressWarnings("unchecked")
+				final T typedResult = (T) result;
+				return typedResult;
+			}
+			if (saneDest == Long.class) {
+				final Long result = number.longValue();
+				@SuppressWarnings("unchecked")
+				final T typedResult = (T) result;
+				return typedResult;
+			}
+			if (saneDest == Short.class) {
+				final Short result = number.shortValue();
+				@SuppressWarnings("unchecked")
+				final T typedResult = (T) result;
+				return typedResult;
+			}
+		}
+
+		// special cases for strings
+		if (src instanceof String) {
+			// source type is String
+			final String s = (String) src;
+			if (s.isEmpty()) {
+				// return null for empty strings
+				return ConversionUtils.getNullValue(dest);
+			}
+
+			// use first character when converting to Character
+			if (saneDest == Character.class) {
+				final Character c = new Character(s.charAt(0));
+				@SuppressWarnings("unchecked")
+				final T result = (T) c;
+				return result;
+			}
+
+			// special case for conversion to enum
+			if (dest.isEnum()) {
+				final T result = ConversionUtils.convertToEnum(s, dest);
+				if (result != null) return result;
+			}
+		}
+		if (saneDest == String.class) {
+			// destination type is String; use Object.toString() method
+			final String sValue = src.toString();
+			@SuppressWarnings("unchecked")
+			final T result = (T) sValue;
+			return result;
+		}
+
+		// wrap the original object with one of the new type, using a constructor
+		try {
+			final Constructor<?> ctor = getConstructor(saneDest, src.getClass());
+			if (ctor == null) return null;
+			@SuppressWarnings("unchecked")
+			final T instance = (T) ctor.newInstance(src);
+			return instance;
+		}
+		catch (final Exception exc) {
+			// TODO: Best not to catch blanket Exceptions here.
+			// no known way to convert
+			return null;
+		}
+	}
+
+	// -- Typed methods --
+
+	@Override
+	public Class<ConversionRequest> getType() {
+		return ConversionRequest.class;
+	}
+
+	// -- Helper methods --
+
+	private Constructor<?> getConstructor(final Class<?> type,
+		final Class<?> argType)
+	{
+		for (final Constructor<?> ctor : type.getConstructors()) {
+			final Class<?>[] params = ctor.getParameterTypes();
+			if (params.length == 1 && ConversionUtils.canCast(argType, params[0])) {
+				return ctor;
+			}
+		}
+		return null;
+	}
+
+	private boolean isArray(final Type type) {
+		return GenericUtils.getComponentClass(type) != null;
+	}
+
+	private boolean isCollection(final Type type) {
+		return ConversionUtils.canCast(GenericUtils.getClass(type),
+			Collection.class);
+	}
+
+	private Object
+		convertToArray(final Object value, final Class<?> componentType)
+	{
+		// First we make sure the value is a collection. This provides the simplest
+		// interface for iterating over all the elements. We use SciJava's
+		// PrimitiveArray collection implementations internally, so that this
+		// conversion is always wrapping by reference, for performance.
+		final Collection<?> items = ArrayUtils.toCollection(value);
+
+		final Object array = Array.newInstance(componentType, items.size());
+
+		// Populate the array by converting each item in the value collection
+		// to the component type.
+		int index = 0;
+		for (final Object item : items) {
+			Array.set(array, index++, convert(item, componentType));
+		}
+		return array;
+	}
+
+	private Object convertToCollection(final Object value,
+		final ParameterizedType pType)
+	{
+		final Collection<Object> collection =
+			createCollection(GenericUtils.getClass(pType));
+		if (collection == null) return null;
+
+		// Populate the collection.
+		final Collection<?> items = ArrayUtils.toCollection(value);
+		// TODO: The following can fail; e.g. "Foo extends ArrayList<String>"
+		final Type collectionType = pType.getActualTypeArguments()[0];
+		for (final Object item : items) {
+			collection.add(convert(item, collectionType));
+		}
+
+		return collection;
+	}
+
+	private Collection<Object> createCollection(final Class<?> type) {
+		// If we were given an interface or abstract class, and not a concrete
+		// class, we attempt to make default implementations.
+		if (type.isInterface() || Modifier.isAbstract(type.getModifiers())) {
+			// We don't have a concrete class. If it's a set or a list, we use
+			// the typical default implementation. Otherwise we won't convert.
+			if (ConversionUtils.canCast(type, List.class)) return new ArrayList<Object>();
+			if (ConversionUtils.canCast(type, Set.class)) return new HashSet<Object>();
+			return null;
+		}
+
+		// Got a concrete type. Instantiate it.
+		try {
+			@SuppressWarnings("unchecked")
+			final Collection<Object> c = (Collection<Object>) type.newInstance();
+			return c;
+		}
+		catch (final InstantiationException exc) {
+			return null;
+		}
+		catch (final IllegalAccessException exc) {
+			return null;
+		}
+	}
+}
