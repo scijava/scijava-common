740f78aea99181f17785e2a52c27367f76a179c6
Mark Hiner <hinerm@gmail.com>
2015-02-11 14:04:27 -0600

    TEMP add marker UI iface

---
+++ src/main/java/org/scijava/help/TroubleshooterUI.java
@@ -0,0 +1,9 @@
+
+package org.scijava.help;
+
+import org.scijava.command.Command;
+
+// FIXME move to another component
+public interface TroubleshooterUI extends Command {
+	// NB: Marker interface
+}
