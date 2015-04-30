dfd2657b111062095be32cf9e92242e04f6e5d0f
Curtis Rueden <ctrueden@wisc.edu>
2015-04-30 14:58:40 -0500

    Add preprocessor that assigns default values
    Unresolved module items should be assigned their default value,
    as given by the ModuleService#getDefaultValue() method, before
    anything else (even module initialization!) happens.

---
+++ src/main/java/org/scijava/module/process/DefaultValuePreprocessor.java
@@ -0,0 +1,78 @@
+/*
+ * #%L
+ * SciJava Common shared library for SciJava software.
+ * %%
+ * Copyright (C) 2009 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.module.process;
+
+import org.scijava.Priority;
+import org.scijava.module.Module;
+import org.scijava.module.ModuleItem;
+import org.scijava.module.ModuleService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ * A preprocessor plugin that populates default parameter values.
+ * <p>
+ * Default values are determined using {@link ModuleService#getDefaultValue}.
+ * </p>
+ *
+ * @author Curtis Rueden
+ */
+@Plugin(type = PreprocessorPlugin.class, priority = Priority.VERY_HIGH_PRIORITY)
+public class DefaultValuePreprocessor extends AbstractPreprocessorPlugin {
+
+	@Parameter
+	private ModuleService moduleService;
+
+	// -- ModuleProcessor methods --
+
+	@Override
+	public void process(final Module module) {
+		for (final ModuleItem<?> input : module.getInfo().inputs()) {
+			assignDefaultValue(module, input);
+		}
+		for (final ModuleItem<?> output : module.getInfo().outputs()) {
+			assignDefaultValue(module, output);
+		}
+	}
+
+	// -- Helper methods --
+
+	private <T> void assignDefaultValue(final Module module,
+		final ModuleItem<T> item)
+	{
+		if (module.isResolved(item.getName())) return;
+		final T defaultValue = moduleService.getDefaultValue(item);
+		if (defaultValue == null) return;
+		item.setValue(module, defaultValue);
+	}
+
+}
