1e491a08c287800dc72fc1dd185cfd1d70bfd38f
Curtis Rueden <ctrueden@wisc.edu>
2016-02-02 22:54:51 -0600

    Add a service for executing main methods

---
+++ src/main/java/org/scijava/main/MainService.java
@@ -0,0 +1,69 @@
+/*
+ * #%L
+ * SciJava Common shared library for SciJava software.
+ * %%
+ * Copyright (C) 2009 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.main;
+
+import org.scijava.service.SciJavaService;
+
+/**
+ * Interface for services which manage dynamic execution of main methods.
+ * 
+ * @author Curtis Rueden
+ */
+public interface MainService extends SciJavaService {
+
+	/**
+	 * Executes registered main classes, in the order they were registered.
+	 * 
+	 * @return The number of main methods which were executed.
+	 */
+	int execMains();
+
+	/** Registers a main class to be executed by {@link #execMains()}. */
+	void addMain(final String className, final String... args);
+
+	/** Gets the registered main classes to execute. */
+	Main[] getMains();
+
+	/** Data structure containing main class and argument values. */
+	interface Main {
+
+		/** Gets the name of the class containing the {@code main} method to run. */
+		String className();
+
+		/** Gets the arguments to pass to the class's {@code main} method. */
+		String[] args();
+
+		/** Runs the {@code main} method with the associated arguments. */
+		void exec();
+	}
+
+}
