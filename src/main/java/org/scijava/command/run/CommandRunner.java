39d5e3b1794954f26c8ad06866267e4a19019a8d
Curtis Rueden <ctrueden@wisc.edu>
2016-02-29 13:47:51 -0600

    Add a ClassRunner for Command plugins
    Migrated from the CommandJavaRunner of scijava/scripting-java.

---
+++ src/main/java/org/scijava/command/run/CommandRunner.java
@@ -0,0 +1,76 @@
+/*
+ * #%L
+ * SciJava Common shared library for SciJava software.
+ * %%
+ * Copyright (C) 2009 - 2016 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.command.run;
+
+import org.scijava.command.Command;
+import org.scijava.command.CommandInfo;
+import org.scijava.command.CommandService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.plugin.PluginService;
+import org.scijava.run.AbstractClassRunner;
+import org.scijava.run.ClassRunner;
+
+/**
+ * Runs the given {@link Command} class.
+ * 
+ * @author Curtis Rueden
+ */
+@Plugin(type = ClassRunner.class)
+public class CommandRunner extends AbstractClassRunner {
+
+	@Parameter
+	private PluginService pluginService;
+
+	@Parameter
+	private CommandService commandService;
+
+	// -- ClassRunner methods --
+
+	@Override
+	public void run(final Class<?> c) {
+		@SuppressWarnings("unchecked")
+		final Class<? extends Command> commandClass =  (Class<? extends Command>) c;
+		final Plugin annotation = c.getAnnotation(Plugin.class);
+		final CommandInfo info = new CommandInfo(commandClass, annotation);
+		pluginService.addPlugin(info);
+		commandService.run(info, true);
+	}
+
+	// -- Typed methods --
+
+	@Override
+	public boolean supports(final Class<?> c) {
+		return Command.class.isAssignableFrom(c);
+	}
+
+}
