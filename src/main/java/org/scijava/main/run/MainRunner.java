8272eb03c8c9fe53af934a1fca58da5a72aeccdf
Curtis Rueden <ctrueden@wisc.edu>
2016-02-29 13:49:41 -0600

    Add a ClassRunner for main methods of classes
    Migrated from the MainJavaRunner of scijava/scripting-java.

---
+++ src/main/java/org/scijava/main/run/MainRunner.java
@@ -0,0 +1,98 @@
+/*
+ * #%L
+ * SciJava Common shared library for SciJava software.
+ * %%
+ * Copyright (C) 2009 - 2016 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.main.run;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.script.ScriptException;
+
+import org.scijava.Priority;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.run.AbstractClassRunner;
+import org.scijava.run.ClassRunner;
+
+/**
+ * Executes the given class's {@code main} method.
+ * 
+ * @author Curtis Rueden
+ */
+@Plugin(type = ClassRunner.class, priority = Priority.LOW_PRIORITY)
+public class MainRunner extends AbstractClassRunner {
+
+	@Parameter(required = false)
+	private LogService log;
+
+	// -- ClassRunner methods --
+
+	@Override
+	public void run(final Class<?> c) throws ScriptException {
+		try {
+			getMain(c).invoke(null, new Object[] { new String[0] });
+		}
+		catch (final IllegalArgumentException exc) {
+			throw new ScriptException(exc);
+		}
+		catch (final IllegalAccessException exc) {
+			throw new ScriptException(exc);
+		}
+		catch (final InvocationTargetException exc) {
+			throw new ScriptException(exc);
+		}
+	}
+
+	// -- Typed methods --
+
+	@Override
+	public boolean supports(final Class<?> c) {
+		return getMain(c) != null;
+	}
+
+	// -- Helper methods --
+
+	private Method getMain(final Class<?> c) {
+		try {
+			return c.getMethod("main", String[].class);
+		}
+		catch (final SecurityException exc) {
+			if (log != null) log.debug(exc);
+			return null;
+		}
+		catch (final NoSuchMethodException exc) {
+			if (log != null) log.debug(exc);
+			return null;
+		}
+	}
+
+}
