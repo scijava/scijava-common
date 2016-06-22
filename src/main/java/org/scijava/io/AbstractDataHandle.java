cb147308801b1f253d9436c530865bc452b1a8f5
Curtis Rueden <ctrueden@wisc.edu>
2016-06-22 16:29:39 -0400

    DataHandle: move default methods into interface

--- src/main/java/org/scijava/io/AbstractDataHandle.java
+++ src/main/java/org/scijava/io/AbstractDataHandle.java
@@ -31,9 +31,6 @@
 
 package org.scijava.io;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 import org.scijava.plugin.AbstractWrapperPlugin;
@@ -46,14 +43,6 @@ import org.scijava.plugin.AbstractWrapperPlugin;
 public abstract class AbstractDataHandle<L extends Location> extends
 	AbstractWrapperPlugin<L> implements DataHandle<L>
 {
-
-	// -- Constants --
-
-	/** Block size to use when searching through the stream. */
-	private static final int DEFAULT_BLOCK_SIZE = 256 * 1024; // 256 KB
-
-	/** Maximum number of bytes to search when searching through the stream. */
-	private static final int MAX_SEARCH_SIZE = 512 * 1024 * 1024; // 512 MB
 
 	// -- Fields --
 
@@ -68,18 +57,8 @@ public abstract class AbstractDataHandle<L extends Location> extends
 	}
 
 	@Override
-	public boolean isLittleEndian() {
-		return getOrder() == ByteOrder.LITTLE_ENDIAN;
-	}
-
-	@Override
 	public void setOrder(final ByteOrder order) {
 		this.order = order;
-	}
-
-	@Override
-	public void setOrder(final boolean little) {
-		setOrder(little ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
 	}
 
 	@Override
@@ -90,186 +69,6 @@ public abstract class AbstractDataHandle<L extends Location> extends
 	@Override
 	public void setEncoding(final String encoding) {
 		this.encoding = encoding;
-	}
-
-	@Override
-	public int read(final ByteBuffer buf) throws IOException {
-		return read(buf, buf.remaining());
-	}
-
-	@Override
-	public int read(final ByteBuffer buf, final int len)
-		throws IOException
-	{
-		final int n;
-		if (buf.hasArray()) {
-			// read directly into the array
-			n = read(buf.array(), buf.arrayOffset(), len);
-		}
-		else {
-			// read into a temporary array, then copy
-			final byte[] b = new byte[len];
-			n = read(b);
-			buf.put(b, 0, n);
-		}
-		return n;
-	}
-
-	@Override
-	public void write(final ByteBuffer buf) throws IOException {
-		write(buf, buf.remaining());
-	}
-
-	@Override
-	public void write(final ByteBuffer buf, final int len)
-		throws IOException
-	{
-		if (buf.hasArray()) {
-			// write directly from the buffer's array
-			write(buf.array(), buf.arrayOffset(), len);
-		}
-		else {
-			// copy into a temporary array, then write
-			final byte[] b = new byte[len];
-			buf.get(b);
-			write(b);
-		}
-	}
-
-	@Override
-	public String readCString() throws IOException {
-		final String line = findString("\0");
-		return line.length() == 0 ? null : line;
-	}
-
-	@Override
-	public String readString(int n) throws IOException {
-		final long avail = length() - offset();
-		if (n > avail) n = (int) avail;
-		final byte[] b = new byte[n];
-		readFully(b);
-		return new String(b, getEncoding());
-	}
-
-	@Override
-	public String readString(final String lastChars) throws IOException {
-		if (lastChars.length() == 1) return findString(lastChars);
-		final String[] terminators = new String[lastChars.length()];
-		for (int i = 0; i < terminators.length; i++) {
-			terminators[i] = lastChars.substring(i, i + 1);
-		}
-		return findString(terminators);
-	}
-
-	@Override
-	public String findString(final String... terminators) throws IOException {
-		return findString(true, DEFAULT_BLOCK_SIZE, terminators);
-	}
-
-	@Override
-	public String findString(final boolean saveString,
-		final String... terminators) throws IOException
-	{
-		return findString(saveString, DEFAULT_BLOCK_SIZE, terminators);
-	}
-
-	@Override
-	public String findString(final int blockSize, final String... terminators)
-		throws IOException
-	{
-		return findString(true, blockSize, terminators);
-	}
-
-	@Override
-	public String findString(final boolean saveString, final int blockSize,
-		final String... terminators) throws IOException
-	{
-		final StringBuilder out = new StringBuilder();
-		final long startPos = offset();
-		long bytesDropped = 0;
-		final long inputLen = length();
-		long maxLen = inputLen - startPos;
-		final boolean tooLong = saveString && maxLen > MAX_SEARCH_SIZE;
-		if (tooLong) maxLen = MAX_SEARCH_SIZE;
-		boolean match = false;
-		int maxTermLen = 0;
-		for (final String term : terminators) {
-			final int len = term.length();
-			if (len > maxTermLen) maxTermLen = len;
-		}
-
-		@SuppressWarnings("resource")
-		final InputStreamReader in =
-			new InputStreamReader(new DataHandleInputStream<>(this), getEncoding());
-		final char[] buf = new char[blockSize];
-		long loc = 0;
-		while (loc < maxLen && offset() < length() - 1) {
-			// if we're not saving the string, drop any old, unnecessary output
-			if (!saveString) {
-				final int outLen = out.length();
-				if (outLen >= maxTermLen) {
-					final int dropIndex = outLen - maxTermLen + 1;
-					final String last = out.substring(dropIndex, outLen);
-					out.setLength(0);
-					out.append(last);
-					bytesDropped += dropIndex;
-				}
-			}
-
-			// read block from stream
-			final int r = in.read(buf, 0, blockSize);
-			if (r <= 0) throw new IOException("Cannot read from stream: " + r);
-
-			// append block to output
-			out.append(buf, 0, r);
-
-			// check output, returning smallest possible string
-			int min = Integer.MAX_VALUE, tagLen = 0;
-			for (final String t : terminators) {
-				final int len = t.length();
-				final int start = (int) (loc - bytesDropped - len);
-				final int value = out.indexOf(t, start < 0 ? 0 : start);
-				if (value >= 0 && value < min) {
-					match = true;
-					min = value;
-					tagLen = len;
-				}
-			}
-
-			if (match) {
-				// reset stream to proper location
-				seek(startPos + bytesDropped + min + tagLen);
-
-				// trim output string
-				if (saveString) {
-					out.setLength(min + tagLen);
-					return out.toString();
-				}
-				return null;
-			}
-
-			loc += r;
-		}
-
-		// no match
-		if (tooLong) throw new IOException("Maximum search length reached.");
-		return saveString ? out.toString() : null;
-	}
-
-	// -- InputStream look-alikes --
-
-	@Override
-	public int read(byte[] b) throws IOException {
-		return read(b, 0, b.length);
-	}
-
-	@Override
-	public long skip(final long n) throws IOException {
-		if (n < 0) return 0;
-		final long remain = length() - offset();
-		final long num = n < remain ? n : remain;
-		seek(offset() + num);
-		return num;
 	}
 
 }
