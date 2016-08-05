38b1e3a9b410febe515cac2779319c5d5eac59df
Curtis Rueden <ctrueden@wisc.edu>
2016-08-04 22:10:54 -0500

    ConversionUtilsTest: remove unused imports

--- src/test/java/org/scijava/util/ConversionUtilsTest.java
+++ src/test/java/org/scijava/util/ConversionUtilsTest.java
@@ -39,15 +39,10 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
