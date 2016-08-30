7021a0ad7375abf2a422dfd94a19fb8ac4e2620a
Richard Domander <rdomander@rvc.ac.uk>
2016-08-30 18:03:55 +0100

    Add a HeadlessUI
    Add a null object pattern style is used, for when UIService is running headless.
    This avoid NullPointerException and similar issues.
    
    Signed-off-by: Curtis Rueden <ctrueden@wisc.edu>

--- src/main/java/org/scijava/ui/DefaultUIService.java
+++ src/main/java/org/scijava/ui/DefaultUIService.java
@@ -68,6 +68,7 @@ import org.scijava.ui.DialogPrompt.MessageType;
 import org.scijava.ui.DialogPrompt.OptionType;
 import org.scijava.ui.DialogPrompt.Result;
 import org.scijava.ui.event.UIShownEvent;
+import org.scijava.ui.headlessUI.HeadlessUI;
 import org.scijava.ui.viewer.DisplayViewer;
 
 /**
@@ -212,6 +213,7 @@ public final class DefaultUIService extends AbstractService implements
 
 	@Override
 	public UserInterface getDefaultUI() {
+		if (isHeadless()) return HeadlessUI.getInstance();
 		if (defaultUI != null) return defaultUI;
 		if (uiList().isEmpty()) return null;
 		return uiList().get(0);
