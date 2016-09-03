e3453f26f2be119cbf0a48f812913df89f45321f
Curtis Rueden <ctrueden@wisc.edu>
2016-09-03 16:29:17 -0500

    StderrLogService: use stdout non-severe messages
    The logging page at http://imagej.net/Logging states that severe logging
    messages (WARN and ERROR levels) are emitted to stderr, while less
    severe ones (INFO and DEBUG levels) are emitted to stdout.
    
    This was not actually true with the default LogService implementation.
    This commit addresses the situation.
    
    Originally, I planned to make this change with SciJava Common 3.0.0,
    because I thought we would need to break backwards compatibility to
    achieve it. But actually, the change was easy to make in a smooth
    manner. SJC3 will see a cleanup of the logging API, but that cleanup
    is not a prerequisite to fixing the situation with stdout + stderr.
    
    Thanks to Florian Jug for spurring this change.

--- src/main/java/org/scijava/log/StderrLogService.java
+++ src/main/java/org/scijava/log/StderrLogService.java
@@ -37,12 +37,26 @@ import org.scijava.service.Service;
 
 /**
  * Implementation of {@link LogService} using the standard error stream.
+ * <p>
+ * Actually, this service is somewhat misnamed now, since it prints {@code WARN}
+ * and {@code ERROR} messages to stderr, but messages at lesser severities to
+ * stdout.
+ * </p>
  * 
  * @author Johannes Schindelin
  * @author Curtis Rueden
  */
 @Plugin(type = Service.class, priority = Priority.LOW_PRIORITY)
 public class StderrLogService extends AbstractLogService {
+
+	@Override
+	protected void log(final int level, final Object msg) {
+		final String prefix = getPrefix(level);
+		final String message = (prefix == null ? "" : prefix + " ") + msg;
+		// NB: Emit severe messages to stderr, and less severe ones to stdout.
+		if (level <= WARN) System.err.println(message);
+		else System.out.println(message);
+	}
 
 	/**
 	 * Prints a message to stderr.
