07e61f08b990a37cb1d99a1c9a8007dd780cec35
Curtis Rueden <ctrueden@wisc.edu>
2016-12-22 16:34:58 -0600

    ModuleInfo: move default method impls to iface
    This has the nice benefit of making the Module interface extend
    the Identifiable, Locatable and Versioned interfaces directly,
    as it should have done in the first place.

--- src/main/java/org/scijava/module/ModuleInfo.java
+++ src/main/java/org/scijava/module/ModuleInfo.java
@@ -31,10 +31,18 @@
 
 package org.scijava.module;
 
+import java.util.List;
+
+import org.scijava.Identifiable;
+import org.scijava.Locatable;
 import org.scijava.UIDetails;
 import org.scijava.Validated;
+import org.scijava.ValidityProblem;
+import org.scijava.Versioned;
 import org.scijava.event.EventService;
 import org.scijava.module.event.ModulesUpdatedEvent;
+import org.scijava.util.ClassUtils;
+import org.scijava.util.VersionUtils;
 
 /**
  * A ModuleInfo object encapsulates metadata about a particular {@link Module}
@@ -49,7 +57,9 @@ import org.scijava.module.event.ModulesUpdatedEvent;
  * @author Aivar Grislis
  * @author Curtis Rueden
  */
-public interface ModuleInfo extends UIDetails, Validated {
+public interface ModuleInfo extends UIDetails, Validated, Identifiable,
+	Locatable, Versioned
+{
 
 	/** Gets the input item with the given name. */
 	ModuleItem<?> getInput(String name);
@@ -116,7 +126,9 @@ public interface ModuleInfo extends UIDetails, Validated {
 	 * means its inputs are supposed to be presented in a non-modal dialog box,
 	 * with {@link Module#run()} being called whenever any of the values change.
 	 */
-	boolean isInteractive();
+	default boolean isInteractive() {
+		return false;
+	}
 
 	/**
 	 * Gets whether the module supports previews. A preview is a quick
@@ -124,7 +136,9 @@ public interface ModuleInfo extends UIDetails, Validated {
 	 * the module with {@link Module#run()}. If this method returns false, then
 	 * calling {@link Module#preview()} will have no effect.
 	 */
-	boolean canPreview();
+	default boolean canPreview() {
+		return false;
+	}
 
 	/**
 	 * Gets whether the module condones cancellation. Strictly speaking, any
@@ -133,7 +147,9 @@ public interface ModuleInfo extends UIDetails, Validated {
 	 * If this method returns false, then calling {@link Module#cancel()} will
 	 * have no effect.
 	 */
-	boolean canCancel();
+	default boolean canCancel() {
+		return true;
+	}
 
 	/**
 	 * Gets whether the module condones headless execution. Strictly speaking,
@@ -141,21 +157,87 @@ public interface ModuleInfo extends UIDetails, Validated {
 	 * declares itself so, but this flag hints that headless execution is likely
 	 * to succeed (if flag is true), or fail (if flag is false).
 	 */
-	boolean canRunHeadless();
+	default boolean canRunHeadless() {
+		return false;
+	}
 
 	/** Gets the function that is called to initialize the module's values. */
-	String getInitializer();
+	default String getInitializer() {
+		return null;
+	}
 
 	/**
 	 * Notifies interested parties that the module info has been modified. This
 	 * mechanism is useful for updating any corresponding user interface such as
 	 * menu items that are linked to the module.
-	 * <p>
-	 * For classes implementing this interface directly, this method should
-	 * publish a {@link ModulesUpdatedEvent} to the event bus (see
-	 * {@link AbstractModuleInfo#update(EventService)} for an example).
-	 * </p>
 	 */
-	void update(EventService eventService);
+	default void update(final EventService eventService) {
+		eventService.publish(new ModulesUpdatedEvent(this));
+	}
 
+	// -- UIDetails methods --
+
+	@Override
+	default String getTitle() {
+		final String title = UIDetails.super.getTitle();
+		if (!title.equals(getClass().getSimpleName())) return title;
+
+		// use delegate class name rather than actual class name
+		final String className = getDelegateClassName();
+		final int dot = className.lastIndexOf(".");
+		return dot < 0 ? className : className.substring(dot + 1);
+	}
+
+	// -- Validated methods --
+
+	@Override
+	default boolean isValid() {
+		return true;
+	}
+
+	@Override
+	default List<ValidityProblem> getProblems() {
+		return null;
+	}
+
+	// -- Identifiable methods --
+
+	@Override
+	default String getIdentifier() {
+		// NB: By default, we assume that the delegate class name uniquely
+		// distinguishes the module from others. If the same delegate class is used
+		// for more than one module, though, it may need to override this method to
+		// provide more differentiating details.
+		return "module:" + getDelegateClassName();
+	}
+
+	// -- Locatable methods --
+
+	@Override
+	default String getLocation() {
+		// NB: By default, we use the location of the delegate class.
+		// If the same delegate class is used for more than one module, though,
+		// it may need to override this method to indicate a different location.
+		try {
+			return ClassUtils.getLocation(loadDelegateClass()).toExternalForm();
+		}
+		catch (final ClassNotFoundException exc) {
+			return null;
+		}
+	}
+
+	// -- Versioned methods --
+
+	@Override
+	default String getVersion() {
+		// NB: By default, we use the version of the delegate class's JAR archive.
+		// If the same delegate class is used for more than one module, though,
+		// it may need to override this method to indicate a different version.
+		try {
+			return VersionUtils.getVersion(loadDelegateClass());
+		}
+		catch (final ClassNotFoundException exc) {
+			return null;
+		}
+	}
 }
