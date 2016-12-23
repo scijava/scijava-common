bf7c0c6614b516b7410d7c8f5552030e26bc4497
Curtis Rueden <ctrueden@wisc.edu>
2016-12-22 18:59:45 -0600

    POMTest: remove unused import

--- src/test/java/org/scijava/util/POMTest.java
+++ src/test/java/org/scijava/util/POMTest.java
@@ -33,7 +33,6 @@ package org.scijava.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
