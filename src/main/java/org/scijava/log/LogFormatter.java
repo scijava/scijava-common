652e1038e3b47aba427bc05c1f83f4bbd9d36e4f
Matthias Arzt <arzt@mpi-cbg.de>
2017-05-16 00:22:23 +0200

    StderrLogService: add LogFormatter for formatting log messages

--- src/main/java/org/scijava/log/LogFormatter.java
+++ src/main/java/org/scijava/log/LogFormatter.java
@@ -8,13 +8,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -31,31 +31,13 @@
 
 package org.scijava.log;
 
-import java.io.PrintStream;
-
-import org.scijava.Priority;
-import org.scijava.plugin.Plugin;
-import org.scijava.service.Service;
-
 /**
- * Implementation of {@link LogService} using the standard error stream.
- * <p>
- * Actually, this service is somewhat misnamed now, since it prints {@code WARN}
- * and {@code ERROR} messages to stderr, but messages at lesser severities to
- * stdout.
- * </p>
- * 
- * @author Johannes Schindelin
- * @author Curtis Rueden
+ * A LogFormatter is used to convert a {@link LogMessage} into a nicely
+ * formatted String.
+ *
+ * @author Matthias Arzt
  */
-@Plugin(type = Service.class, priority = Priority.LOW_PRIORITY)
-public class StderrLogService extends AbstractLogService {
+public interface LogFormatter {
 
-	private final LogFormatter formatter = new DefaultLogFormatter();
-
-	@Override
-	public void alwaysLog(final int level, final Object msg, final Throwable t) {
-		final PrintStream out = (level <= LogLevel.WARN) ? System.err : System.out;
-		out.print(formatter.format(new LogMessage(level, msg, t)));
-	}
+	String format(LogMessage message);
 }
