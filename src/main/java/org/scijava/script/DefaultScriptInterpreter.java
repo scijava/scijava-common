8c84c5660eadd1ec7539bcc0848590effefed47b
Curtis Rueden <ctrueden@wisc.edu>
2017-05-17 10:32:41 -0500

    Move internal class into DefaultScriptInterpreter

--- src/main/java/org/scijava/script/DefaultScriptInterpreter.java
+++ src/main/java/org/scijava/script/DefaultScriptInterpreter.java
@@ -42,6 +42,7 @@ import javax.script.ScriptException;
 import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.prefs.PrefService;
+import org.scijava.util.LastRecentlyUsed;
 
 /**
  * The default implementation of a {@link ScriptInterpreter}.
@@ -363,4 +364,113 @@ public class DefaultScriptInterpreter implements ScriptInterpreter {
 		return null;
 	}
 
+	// -- Helper classes --
+	
+	/** Container for a script language's interpreter history. */
+	private static class History {
+
+		@SuppressWarnings("unused")
+		protected static final long serialVersionUID = 2L;
+
+		private static final String PREFIX = "History.";
+		private final int MAX_ENTRIES = 1000;
+
+		private final PrefService prefs;
+		private final String name;
+		private final LastRecentlyUsed<String> entries =
+			new LastRecentlyUsed<>(MAX_ENTRIES);
+		private String currentCommand = "";
+		private int position = -1;
+
+		/**
+		 * Constructs a history object for a given scripting language.
+		 * 
+		 * @param name the name of the scripting language
+		 */
+		public History(final PrefService prefs, final String name) {
+			this.prefs = prefs;
+			this.name = name;
+		}
+
+		/**
+		 * Read back a persisted history.
+		 */
+		public void read() {
+			entries.clear();
+			for (final String item : prefs.getIterable(getClass(), PREFIX + name)) {
+				entries.addToEnd(item);
+			}
+		}
+
+		/**
+		 * Persist the history.
+		 * 
+		 * @see PrefService
+		 */
+		public void write() {
+			prefs.putIterable(getClass(), entries, PREFIX + name);
+		}
+
+		/**
+		 * Adds the most recently issued command.
+		 * 
+		 * @param command the most recent command to add to the history
+		 */
+		public void add(final String command) {
+			entries.add(command);
+			position = -1;
+			currentCommand = "";
+		}
+
+		public boolean replace(final String command) {
+			if (position < 0) {
+				currentCommand = command;
+				return false;
+			}
+			return entries.replace(position, command);
+		}
+
+		/**
+		 * Navigates to the next (more recent) command.
+		 * <p>
+		 * This method wraps around, i.e. it returns {@code null} when there is no
+		 * more-recent command in the history.
+		 * </p>
+		 * 
+		 * @return the next command
+		 */
+		public String next() {
+			position = entries.next(position);
+			return position < 0 ? currentCommand : entries.get(position);
+		}
+
+		/**
+		 * Navigates to the previous (i.e less recent) command.
+		 * <p>
+		 * This method wraps around, i.e. it returns {@code null} when there is no
+		 * less-recent command in the history.
+		 * </p>
+		 * 
+		 * @return the previous command
+		 */
+		public String previous() {
+			position = entries.previous(position);
+			return position < 0 ? currentCommand : entries.get(position);
+		}
+
+		@Override
+		public String toString() {
+			final StringBuilder builder = new StringBuilder();
+			int pos = -1;
+			for (;;) {
+				pos = entries.previous(pos);
+				if (pos < 0) break;
+				if (builder.length() > 0) builder.append(" -> ");
+				if (this.position == pos) builder.append("[");
+				builder.append(entries.get(pos));
+				if (this.position == pos) builder.append("]");
+			}
+			return builder.toString();
+		}
+	}
 }
