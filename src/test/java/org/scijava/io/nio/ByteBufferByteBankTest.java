969cf3a85d076439e58e6946ad0cedd71242a4f6
Curtis Rueden <ctrueden@wisc.edu>
2017-07-10 14:58:26 -0500

    ByteBufferByteBank: test support for read-only

---
+++ src/test/java/org/scijava/io/nio/ByteBufferByteBankTest.java
@@ -0,0 +1,75 @@
+/*
+ * #%L
+ * SciJava Common shared library for SciJava software.
+ * %%
+ * Copyright (C) 2009 - 2016 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.io.nio;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link ByteBufferByteBank}.
+ *
+ * @author Curtis Rueden
+ */
+public class ByteBufferByteBankTest {
+
+	@Test
+	public void testReadOnlyDefault() {
+		final ByteBufferByteBank bank = new ByteBufferByteBank();
+		assertFalse(bank.isReadOnly());
+	}
+
+	@Test
+	public void testReadOnlyAllocate() {
+		final ByteBufferByteBank bank = new ByteBufferByteBank(
+			ByteBuffer::allocate);
+		assertFalse(bank.isReadOnly());
+
+		final ByteBufferByteBank readOnlyBank = new ByteBufferByteBank(
+			capacity -> ByteBuffer.allocate(capacity).asReadOnlyBuffer());
+		assertTrue(readOnlyBank.isReadOnly());
+	}
+
+	@Test
+	public void testReadOnlyAllocateDirect() {
+		final ByteBufferByteBank bank = new ByteBufferByteBank(
+			ByteBuffer::allocateDirect);
+		assertFalse(bank.isReadOnly());
+
+		final ByteBufferByteBank readOnlyBank = new ByteBufferByteBank(
+			capacity -> ByteBuffer.allocateDirect(capacity).asReadOnlyBuffer());
+		assertTrue(readOnlyBank.isReadOnly());
+	}
+}
