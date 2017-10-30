a00f50de7e84b3d04b9529df839ac92043d2652c
Curtis Rueden <ctrueden@wisc.edu>
2017-10-30 16:17:28 -0500

    Travis: decrypt GPG keypair via travis-build.sh
    Rather than having the .travis.yml declare that it should be
    done before_install, we have more control if it is done as part
    of the build itself. For one thing, we can use if statements
    without being at the mercy of Travis's YAML support.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh
+sh travis-build.sh $encrypted_d2fbfc37eea9_key $encrypted_d2fbfc37eea9_iv
