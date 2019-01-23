80f96e74b2b6cbcf02662ba8c1b94e189fb37baf
Gabriel Einsdorf <gabriel.einsdorf@knime.com>
2019-01-23 15:44:53 +0100

    BytesLocation: add test for setName()

--- src/test/java/org/scijava/io/location/BytesLocationTest.java
+++ src/test/java/org/scijava/io/location/BytesLocationTest.java
@@ -9,13 +9,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -36,10 +36,12 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.scijava.io.ByteArrayByteBank;
+import org.scijava.util.ByteArray;
 
 /**
  * Tests {@link BytesLocation}.
- * 
+ *
  * @author Curtis Rueden
  */
 public class BytesLocationTest {
@@ -70,6 +72,33 @@ public class BytesLocationTest {
 		final byte[] expectedDigits = new byte[digits.length];
 		System.arraycopy(digits, offset, expectedDigits, 0, length);
 		assertArrayEquals(expectedDigits, testDigits);
+	}
+
+	/**
+	 * Tests getName()
+	 */
+	@Test
+	public void getNameTest() {
+
+		final BytesLocation loc1 = new BytesLocation(0);
+		assertEquals(loc1.defaultName(), loc1.getName());
+		assertEquals("Location.defaultName", loc1.defaultName());
+
+		final String expectedName = "test.name";
+		BytesLocation loc2 = new BytesLocation(0, expectedName);
+		assertEquals(expectedName, loc2.getName());
+
+		loc2 = new BytesLocation(new byte[0], expectedName);
+		assertEquals(expectedName, loc2.getName());
+
+		loc2 = new BytesLocation(new ByteArray(), expectedName);
+		assertEquals(expectedName, loc2.getName());
+
+		loc2 = new BytesLocation(new ByteArrayByteBank(), expectedName);
+		assertEquals(expectedName, loc2.getName());
+
+		loc2 = new BytesLocation(new byte[0], 0, 0, expectedName);
+		assertEquals(expectedName, loc2.getName());
 	}
 
 }
