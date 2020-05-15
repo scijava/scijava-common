656521b61b715e06ad22729420dd37f8eeb361db
Curtis Rueden <ctrueden@wisc.edu>
2020-05-15 12:19:15 -0500

    Update to parsington 2.0.0
    This is a breaking package rename on the parsington side, to avoid
    split packages. It facilitates use from JPMS-modularized projects.

--- src/main/java/org/scijava/parse/DefaultParseService.java
+++ src/main/java/org/scijava/parse/DefaultParseService.java
@@ -37,7 +37,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.scijava.parse.eval.DefaultEvaluator;
+import org.scijava.parsington.Variable;
+import org.scijava.parsington.eval.DefaultEvaluator;
 import org.scijava.plugin.Plugin;
 import org.scijava.service.AbstractService;
 import org.scijava.service.Service;
