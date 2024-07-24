055a449826c6a541d84c458575a8497e952d31c6
Curtis Rueden <ctrueden@wisc.edu>
2024-07-24 12:00:02 -0500

    CI: Tweak Linux-only deployment logic

--- .github/build.sh
+++ .github/build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-build.sh
-# NB: Only the Linux CI node should deploy build artifacts.
-NO_DEPLOY=$(test "$(uname)" = Linux || echo 1) sh ci-build.sh
+sh ci-build.sh
