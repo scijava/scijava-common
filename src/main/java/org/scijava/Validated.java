4fc2b14f72ac173827764bcfbd756a820a72b87f
Curtis Rueden <ctrueden@wisc.edu>
2025-05-12 16:32:35 -0500

    Update copyright blurbs for 2025

--- src/main/java/org/scijava/Validated.java
+++ src/main/java/org/scijava/Validated.java
@@ -2,7 +2,7 @@
  * #%L
  * SciJava Common shared library for SciJava software.
  * %%
- * Copyright (C) 2009 - 2024 SciJava developers.
+ * Copyright (C) 2009 - 2025 SciJava developers.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
